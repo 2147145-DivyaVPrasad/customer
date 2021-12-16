62ebc85bd482ab2a441dbd63105b17eecd7345e0
2147145-DivyaVPrasad <95071495+2147145-DivyaVPrasad@users.noreply.github.com>
2021-12-17 01:50:25 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,38 @@
+<?php
+$insert = false;
+if(isset($_POST['name'])){
+ // Set connection variables
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password);
+
+    // Check for connection success
+    if(!$con){
+        die("connection to this database failed due to" . mysqli_connect_error());
+    }
+    else{
+    echo "Success connecting to the db";}
+
+    // Collect post variables
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $website = $_POST['website'];
+    $message = $_POST['message'];
+    $sql = "INSERT INTO `techzonation`.`techzontable` (`name`, `email`, `phone`, `website`, `message`) VALUES ('$name', '$email', '$phone', '$website', '$message');";
+if($con->query($sql) == true){
+    //echo "Successfully inserted";
+    $insert = true;
+}
+else 
+{
+            echo "ERROR: $sql <br> $con->error";
+
+}
+
+
+  $con->close();}
+  ?>
